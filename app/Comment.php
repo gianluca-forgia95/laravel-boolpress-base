a9dbc1f25830df59b0424402fbe6c56a84a070c3
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-24 14:17:10 +0200

    collego i due model Post e Comment con la loro relazione

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $guarded = [];
+    //Function post per collegare piu commenti ad un unico post
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $guarded = [];
+    //Function Comments per collegare 1 post a piu commenti
+    public function comments() 
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
