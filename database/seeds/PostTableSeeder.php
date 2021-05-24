abb941396be040f73329ed2b23144eddcdfddfde
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-24 14:40:32 +0200

    creo 10 fake posts

---
+++ database/seeds/PostTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+use App\Post;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run( Faker $faker)
+    {
+        for ( $i = 0; $i < 10; $i++) {
+            $newPost = new Post();
+            $newPost->title = $faker->sentence();
+            $newPost->date = $faker->date();
+            $newPost->content = $faker->text();
+            $newPost->img = $faker->imageUrl(640, 480, 'animals', true);
+            $newPost->slug = Str::slug( $newPost->title , '-');
+            $newPost->published = rand(0,1);
+            $newPost->save();
+        }
+    }
+}
+
+
